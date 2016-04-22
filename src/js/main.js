a0e60e5259ee28a2648f271f4e2eefabf6061f51
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-04-22 14:28:47 -0400

    setting up structure for main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,23 @@ import { Link, hashHistory } from 'react-router';
 export default class Main extends Component {
   render(){
     return (
-      <div></div>
+      <div className="main-wrapper">
+      	<div className="main-logo">
+      		<img src="http://www.fillmurray.com/30/30" alt="logo-holder"/>
+      	</div>
+
+      	<div className="main-nav">
+      		<Link  to="/">Home</Link>
+      		<Link  to="/">About</Link>
+      		<Link  to="/">Profile</Link>
+      	</div>
+
+      	<div className="main-sign-up-in">
+      		<Link  to="/">Sign Up</Link>
+      		<Link  to="/">Sign In</Link>
+      	</div>
+
+      </div>
     )
   }
 }
