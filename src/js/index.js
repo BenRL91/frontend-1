3a2a1c3f628ec5a11380a4bd459d7a485a78d92a
Ben Lee <leeohsunshine@gmail.com>
2016-04-25 10:30:00 -0400

    Created Login form (non-functioning) and set the /login route to the Login component.

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ import { render } from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import Main from './main';
 import Home from './home';
+import Login from './login';
 
 render((
   <Router history={ hashHistory }>
     <Route path='/' component={ Main }>
       <IndexRoute component={ Home }/>
+      <Route path='/login' component={ Login }/>
       <Route path='/profile/:user_name'/>
     </Route>
   </Router>
--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import { Link, hashHistory } from 'react-router';
+import { ajax, ajaxSetup } from 'jquery';
 
 export default class Login extends Component {
   render(){
+
+    login(user_credentials){
+
+      ajax({
+        url: 'http://'/*herokusomething*/'/login',
+        type: 'POST',
+        data: user_credentials,
+
+      })
+    }
+
     return (
-      <div></div>
+      <div className='login-wrapper'>
+        <form>
+          {/*Have not decided on names yet*/}
+          <label>
+            Username:
+            <input
+              type='text'
+              name='user_name'
+              placeholder='Type Your Username'/>
+          </label>
+          <label>
+            Password:
+            <input
+              type='password'
+              name='password'
+              placeholder='Type Your Password'/>
+          </label>
+          <button>Log In</button>
+        </form>
+      </div>
     )
   }
 }
