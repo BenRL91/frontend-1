a7d56383224c0009622745530a6d263ab9e6f86f
Ben Lee <leeohsunshine@gmail.com>
2016-05-08 23:46:18 -0400

    Trip details oganization.

--- src/js/trip_details.js
+++ src/js/trip_details.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Link, hashHistory } from 'react-router';
 import { ajax } from 'jquery';
 import cookie from 'js-cookie';
-import $ from 'jquery';
-import Modal from './modal';
-import Login from './login';
-
 
 export default class TripDetails extends Component {
 
@@ -13,8 +9,9 @@ export default class TripDetails extends Component {
   constructor(...args){
   super(...args);
   this.state = {
-    current_trip: {},
-    current_user: {},
+    current_trip: null,
+    current_user: null,
+    driver: null,
     loading: true
     }
   }
@@ -22,66 +19,31 @@ export default class TripDetails extends Component {
 
   componentWillMount(){
   	let respA;
-    let user_id;
-    let user = cookie.getJSON('current_user')
-    if (user){
-      user_id = user.current_user.id;
-    }else {
-      user_id = null;
-    }
-    ajax(`http://salty-river-31528.herokuapp.com/hosts/${this.props.params.trip_id}`)
-    .then( resp => {
-      console.log(resp)
-      if (user_id === resp.hosts.user_id ){
-        $('.edit-btn').removeClass('hidden');
-        $('.book-btn').addClass('hidden');
-      }
-
-    })
-
-	ajax(`http://salty-river-31528.herokuapp.com/hosts/${this.props.params.trip_id}`)
+    let { trip_id } = this.props.params;
+    let current_user = cookie.getJSON('current_user')
+    ? cookie.getJSON('current_user').current_user
+    : null
+	ajax(`http://salty-river-31528.herokuapp.com/hosts/${trip_id}`)
 		.then( resp => {
 	        respA = resp;
 	        this.setState({current_trip: resp.hosts})
 	        return ajax(`https://salty-river-31528.herokuapp.com/profile/${resp.hosts.user_id}`);
-		})
+		}).fail(e => { console.log(e)})
 		.then( respB => {
       console.log(respB)
-			this.setState({current_user: respB.user, loading: false})
-			cookie.set('saved_trip', {trip_id: this.props.params.trip_id})
+			this.setState({driver: respB.user, loading: false})
+			cookie.set('saved_trip', {trip_id})
 		}).fail(e => { console.log( e) })
 
   }
-
-
-  // check auth
-  // IF logged in SHOW EDIT BUTTON
-  // ELSE dont show edit button
-  //***** CHECK HERE IF USER CREATED THE TRIP THEYRE VIEWING,
-  // IF SO MAKE AN EDIT BUTTON AVAILABLE**** 1st FRIDAY****
-
-
-
-			// request to get info on the driver
-
-  // checkProf(){
-  //   let user_id = cookie.getJSON('current_user').current_user.id
-  //   ajax(`https://salty-river-31528.herokuapp.com/profile/${user_id}`).then( resp => {
-  //     console.log(resp)
-  //   })
-  // }
-
 renderLoading(){
   return (
     <div>Loading...</div>
     )
 }
 renderPage(){
-      let trip    = this.state.current_trip;
-    let user    = this.state.current_user;
-    let trip_id = this.props.params.trip_id;
-
-    console.log('user', user.pictures)
+    let { current_trip, driver} = this.state;
+    let { trip_id } = this.props.params;
     return (
       <div className="trip-details-wrapper">
 
@@ -93,41 +55,41 @@ renderPage(){
 
          <div className="trip-details-flex">
            <div className="trip-details-departing">
-            <b>{trip.departing_city}</b> <br/> {trip.date_leave}
+            <b>{current_trip.departing_city}</b> <br/> {current_trip.date_leave}
            </div>
 
             <div className="direction-line">  </div> <div className="direction-arrow"> ► </div>
 
             <div className="trip-details-destination">
-            <b>{trip.destination}</b> <br/> {trip.date_arrive}
+            <b>{current_trip.destination}</b> <br/> {current_trip.date_arrive}
             </div>
             </div>
 
           <div className="trip-details-seats">
-          <b>{trip.seats_available}</b>
+          <b>{current_trip.seats_available}</b>
           <br/>  seats available
           </div>
 
           <div className="trip-details-price">
-          <span>${trip.seat_price}</span>
+          <span>${current_trip.seat_price}</span>
           </div>
-    
+
 
 
         <div className="trip-details-driver">
           <div className="trip-details-driver-flex">
 
-            <img src={user.pictures[0].image_url}/>
+            <img src={driver.pictures[0].image_url}/>
 
             <div className="driver-content-flex">
               <div>
-                <span className="trip-details-driver-name">{user.first_name}</span> 
+                <span className="trip-details-driver-name">{driver.first_name}</span>
                   <br/>
-                <Link className="trip-details-driver-link" to={`/profile/${trip.user_id}`}> view drivers profile </Link>
+                <Link className="trip-details-driver-link" to={`/profile/${driver.id}`}> view drivers profile </Link>
               </div>
 
               <div className="trip-details-para">
-                Trip Description: {trip.comments}
+                Trip Description: {current_trip.comments}
                </div>
             </div>
 
@@ -140,7 +102,7 @@ renderPage(){
 
 
 
-           <Link className='book-btn' to={`/ridertripbooking/${trip.id}`}> Book Trip → </Link>
+           <Link className='book-btn' to={`/riderbooking/${current_trip.id}`}> Book Trip → </Link>
      </div>
     )
   }
@@ -152,14 +114,3 @@ renderPage(){
     )
 }
 }
-
-
-
-          // <Modal show={this.state.showLogin} onCloseRequest={::this.hideLoginHandler}>
-          //    <Login/>
-          // </Modal>
-
-
-
-// <Link to="/profile"> go to driver profile </Link>
-// NEED TO SPECIFY THAT YOURE GOING TO THAT SPECIFIC DRIVERS PROFILE//
