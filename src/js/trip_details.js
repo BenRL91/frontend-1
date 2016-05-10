8d1c1e6b48e9d29c6e0b5ee85037c50b34a890e7
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-05-10 13:45:16 -0400

    sassing everything, book/edit trip interchangable

--- src/js/trip_details.js
+++ src/js/trip_details.js
@@ -58,7 +58,9 @@ renderEditLink(){
   let { current_trip } = this.state;
   if(this.allowEdit()){
     return(
-      <Link className="edit-btn" to={`/edittrip/${current_trip.id}`}> + EDIT YOUR TRIP </Link>
+      <Link className="book-btn" to={`/edittrip/${current_trip.id}`}> 
+      + EDIT YOUR TRIP 
+      </Link>
     )
   }else {
     return;
@@ -69,59 +71,61 @@ renderPage(){
     let { trip_id } = this.props.params;
     return (
       <div className="trip-details-wrapper">
-        {::this.renderEditLink()}
         <div className="trip-details">
 
-         <div className="trip-details-flex">
-           <div className="trip-details-departing">
-            <b>{current_trip.departing_city}</b> <br/> {current_trip.date_leave}
-           </div>
+        <div className="trip-details-flex">
 
-            <div className="direction-line">  </div> <div className="direction-arrow"> ► </div>
 
-            <div className="trip-details-destination">
-            <b>{current_trip.destination}</b> <br/> {current_trip.date_arrive}
+          <div className="trip-details-departing">
+            <div>
+              <i className="fa fa-circle-o" aria-hidden="true"></i>
+              <b>{current_trip.departing_city}</b> 
             </div>
+           {current_trip.date_leave}
+          </div>
+        <br/>
+          <div className="trip-details-destination">
+            <div>
+              <i className="fa fa-bullseye" aria-hidden="true"></i>
+              <b>{current_trip.destination}</b> 
             </div>
+           {current_trip.date_arrive}
+         </div>
 
-          <div className="trip-details-seats">
-          <b>{current_trip.seats_left}</b>
-          <br/>  seats available
+
+        </div>
+
+        <div className="trip-details-seats">
+          <div>{current_trip.seats_left} seats available for ${current_trip.seat_price} each
           </div>
 
-          <div className="trip-details-price">
-          <span>${current_trip.seat_price}</span>
+          <div className="book-edit">
+           <Link className='book-btn' to={`/riderbooking/${current_trip.id}`}> Book Trip → </Link>
+           {::this.renderEditLink()}
           </div>
-
+        </div>
 
 
         <div className="trip-details-driver">
           <div className="trip-details-driver-flex">
-
-            <img src={driver.pictures[0].image_url}/>
-
             <div className="driver-content-flex">
-              <div>
-                <span className="trip-details-driver-name">{driver.first_name}</span>
-                  <br/>
-                <Link className="trip-details-driver-link" to={`/profile/${driver.id}`}> view drivers profile </Link>
+
+              <img src={driver.pictures[0].image_url}/>
+              <span className="trip-details-driver-name">{driver.first_name} {driver.last_name}</span>
+              <Link className="trip-details-driver-link" to={`/profile/${driver.id}`}> view drivers profile </Link>
               </div>
+    
 
               <div className="trip-details-para">
                 Trip Description: {current_trip.comments}
                </div>
-            </div>
+
 
         </div>
         </div>
 
 
       </div>
-
-
-
-
-           <Link className='book-btn' to={`/riderbooking/${current_trip.id}`}> Book Trip → </Link>
      </div>
     )
   }
